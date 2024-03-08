982cbad20fde60469132108d3e886604887e7fb7
Kawin <tonnum.kawin@gmail.com>
2024-03-08 15:58:33 +0700

    add Input Pop up to input Data type, Variable Name, Value (Only typing and Data type Have only Int, Double, String case sensitive)

---
+++ T-Rex/src/Input_main_test.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Input_main_test {
+    public static void main(String[] args) {
+        Input input = new Input();
+    }
+}
