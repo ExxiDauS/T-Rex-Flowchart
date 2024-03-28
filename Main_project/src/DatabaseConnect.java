56f0905211e2e3a2d5a10862fb8bab7684c29a6a
exxidaus <kpinthawornrak@gmail.com>
2024-03-29 00:38:50 +0700

    feat: Added login method and cleanup the code a little bit.

--- Main_project/src/DatabaseConnect.java
+++ Main_project/src/DatabaseConnect.java
@@ -1,27 +1,32 @@
 import java.sql.*;
 
-public class TestConnection {
+public class DatabaseConnect {
+//    DB credentials.
     static final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
     static final String DB_URL = "jdbc:mariadb://161.246.127.24:9004/clua7yac5000cbsmnfvj64hef";
-
-    public static void main(String[] args) {
+    
+    static final String USER = "clua7yac3000absmnajq5d7jl";
+    static final String PASS = "kVGzhZ64mXAGplcwmtUL3Ub4";
+    
+    public boolean login(String username, String password){
         String sql = "select * from User";
-        try (   Connection connect = DriverManager.getConnection(
+        try (Connection connect = DriverManager.getConnection(
                     "jdbc:mariadb://161.246.127.24:9004/clua7yac5000cbsmnfvj64hef", 
                     "clua7yac3000absmnajq5d7jl", "kVGzhZ64mXAGplcwmtUL3Ub4");
                 Statement s = connect.createStatement();
                 ResultSet rec = s.executeQuery(sql);){
             Class.forName("org.mariadb.jdbc.Driver");
-//            System.out.println("Connecting to a selected database...");
             System.out.println("Connected database successfully...");
-            while (rec.next()) {
-                System.out.print("User ID : " + rec.getInt(1) + " ");
-                System.out.print("Name : " + rec.getString(2) + " ");
-                System.out.print("Role : " + rec.getString(3) + " ");
-                System.out.println("Score : " + rec.getDouble(4));
+            while (rec.next()) {                
+                if ((username.equals(rec.getString("Username"))) && (password.equals(rec.getString("Password")))) {
+                    System.out.println("Login...");
+                    return true;
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        System.out.println("Error! Incorrect username or password.");
+        return false;
     }
 }
--- Main_project/src/Main.java
+++ Main_project/src/Main.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        new Flowchart();
+        DatabaseConnect test = new DatabaseConnect();
+        System.out.println(test.login("TravisZa", "OOPInwZa"));
     }
 }
