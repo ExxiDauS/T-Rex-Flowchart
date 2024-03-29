cc9431b8ff00013b850546fcd8bf5ea0b2299aa2
exxidaus <kpinthawornrak@gmail.com>
2024-03-29 23:02:50 +0700

    feat: Added 50% grader.
    Waiting to connect testcase with db.

---
+++ Main_project/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World1");
+        System.out.println("Hello World2");
+        System.out.println("Hello World3");
+        System.out.println("Hello World4");
+    }
+}
