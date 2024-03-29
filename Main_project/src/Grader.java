39cff030a21c1c86a1fed0dfefa5c98efd26baae
exxidaus <kpinthawornrak@gmail.com>
2024-03-29 23:02:50 +0700

    feat: Added 50% grader.
    Waiting to connect testcase with db.

---
+++ Main_project/src/Grader.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Grader {
+    public ArrayList checkResult(File f) {
+        ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", "java " + f.getAbsolutePath());
+        ArrayList codeResult = new ArrayList();
+//        TODO : connect testcase with DB
+        ArrayList testcase = new ArrayList();
+        try {
+            Process p = builder.start();
+            BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+            int i = 0;
+            while ((line = r.readLine()) != null) {
+//                for test
+//                    System.out.println(line);
+//                    System.out.println(testcase.get(i).equals(line));
+                    if (i < testcase.size()) {
+                        codeResult.add(testcase.get(i).equals(line));
+                    }
+                    i += 1;
+                }
+            return codeResult;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- Main_project/src/Main.java
+++ Main_project/src/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
