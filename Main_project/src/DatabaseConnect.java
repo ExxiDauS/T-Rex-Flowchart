547c589ce3664ab701a6760d3d61688585e3f96b
exxidaus <kpinthawornrak@gmail.com>
2024-03-29 20:24:21 +0700

    feat: Added insert/get problem method.

--- Main_project/src/DatabaseConnect.java
+++ Main_project/src/DatabaseConnect.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.Arrays;
 
 public class DatabaseConnect {
 //    DB credentials.
@@ -28,5 +29,48 @@ public class DatabaseConnect {
         }
         System.out.println("Error! Incorrect username or password.");
         return false;
+    }
+    
+    public void insertProblem(String description, String imgUrl, Arrays cases[], String titleName){
+        String sql = "INSERT INTO Question VALUES(ID, '{\"description\": " + description + ", \"imgUrl\": " + imgUrl
+                + ", \"cases\": " + Arrays.toString(cases) + ", \"titleName\": " + titleName + "}');";
+        try (Connection connect = DriverManager.getConnection(
+                    "jdbc:mariadb://161.246.127.24:9004/clua7yac5000cbsmnfvj64hef", 
+                    "clua7yac3000absmnajq5d7jl", "kVGzhZ64mXAGplcwmtUL3Ub4");
+                Statement s = connect.createStatement();
+                ){
+            Class.forName("org.mariadb.jdbc.Driver");
+            System.out.println("Connected database successfully...");
+            int i = s.executeUpdate(sql);
+            if (i > 0) {
+                System.out.println("ROW INSERTED");
+            } else {
+                System.out.println("ROW NOT INSERTED");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public String getProblem(int id){
+        String sql = "SELECT * FROM Question";
+        try (Connection connect = DriverManager.getConnection(
+                    "jdbc:mariadb://161.246.127.24:9004/clua7yac5000cbsmnfvj64hef", 
+                    "clua7yac3000absmnajq5d7jl", "kVGzhZ64mXAGplcwmtUL3Ub4");
+                Statement s = connect.createStatement();
+                ResultSet rec = s.executeQuery(sql);){
+            Class.forName("org.mariadb.jdbc.Driver");
+            System.out.println("Connected database successfully...");
+            while (rec.next()) {                
+                if (rec.getInt("ID") == id) {
+                    System.out.println("Get problem successfully.");
+                    return rec.getString("Problem");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Please check problem ID and try again.");
+        return null;
     }
 }
--- Main_project/src/Main.java
+++ Main_project/src/Main.java
@@ -2,6 +2,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         DatabaseConnect test = new DatabaseConnect();
-        System.out.println(test.login("TravisZa", "OOPInwZa"));
+        test.insertProblem("Travis have 5 baht give Scott 10 baht. How much money does Travis have left?"
+                , "https://media.discordapp.net/attachments/1156324979705651381/1219016871291129936/178727725_1587958818057083_5793406160138075441_n.png?ex=6612ff6e&is=66008a6e&hm=8b6f9f4ed3f35687ce3cc0430d918d176c3a6e2b959c66c4b45697b4c3f85906&=&format=webp&quality=lossless&width=198&height=146"
+                , null, "Travis so rich!!!");
+        System.out.println(test.getProblem(6));
     }
 }
