b91088bdf99584a6e467b46dee01db740b4e6a01
Pubeth <66070158@kmitl.ac.th>
2024-03-29 16:52:54 +0700

    Forget to commit DatabaseConnect.java

--- Main_project/src/StartShape.java
+++ Main_project/src/StartShape.java
@@ -1,6 +1,33 @@
 import java.awt.*;
 import javax.swing.*;
+import java.io.*;
 
 public class StartShape extends Shape{
 
+    @Override
+    public void convertToCode(File f) {
+        if (f.exists()){
+            f.delete();
+            try (FileWriter fw = new FileWriter(f)){
+                String fileNameWithOutExt = f.getName().replaceFirst("[.][^.]+$", "");
+                fw.write("public class " + fileNameWithOutExt + " {\n");
+                fw.write("public static void main(String[] args) {\n");
+                fw.write("try { \n");
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            try (FileWriter fw = new FileWriter(f)){
+                String fileNameWithOutExt = f.getName().replaceFirst("[.][^.]+$", "");
+                fw.write("public class " + fileNameWithOutExt + " {\n");
+                fw.write("public static void main(String[] args) {\n");
+                fw.write("try { \n");
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
