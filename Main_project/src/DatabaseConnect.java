05c1f806e3cb21c7db37e4c04cd4189da429f445
exxidaus <kpinthawornrak@gmail.com>
2024-03-30 15:22:56 +0700

    feat: Added connection to testcases.

--- Main_project/src/DatabaseConnect.java
+++ Main_project/src/DatabaseConnect.java
@@ -73,4 +73,83 @@ public class DatabaseConnect {
         System.out.println("Please check problem ID and try again.");
         return null;
     }
+    
+    public void insertTestcases(ArrayList input[], ArrayList output[]){
+        String sql = "INSERT INTO Testcases VALUES(ID, " 
+                + Arrays.toString(input) + ", " + Arrays.toString(output) + ");";
+        try (Connection connect = DriverManager.getConnection(
+                    "jdbc:mariadb://161.246.127.24:9004/clua7yac5000cbsmnfvj64hef", 
+                    "clua7yac3000absmnajq5d7jl", "kVGzhZ64mXAGplcwmtUL3Ub4");
+                Statement s = connect.createStatement();
+                ){
+            Class.forName("org.mariadb.jdbc.Driver");
+            System.out.println("Connected database successfully...");
+            int i = s.executeUpdate(sql);
+            if (i > 0) {
+                System.out.println("ROW INSERTED");
+            } else {
+                System.out.println("ROW NOT INSERTED");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public ArrayList getInput(int questionID){
+        String sql = "SELECT * FROM Testcases";
+        String tmp = "";
+        ArrayList input = new ArrayList();
+        try (Connection connect = DriverManager.getConnection(
+                    "jdbc:mariadb://161.246.127.24:9004/clua7yac5000cbsmnfvj64hef", 
+                    "clua7yac3000absmnajq5d7jl", "kVGzhZ64mXAGplcwmtUL3Ub4");
+                Statement s = connect.createStatement();
+                ResultSet rec = s.executeQuery(sql);){
+            Class.forName("org.mariadb.jdbc.Driver");
+            System.out.println("Connected database successfully...");
+            while (rec.next()) {                
+                if (rec.getInt("ID") == questionID) {
+                    System.out.println("Get Input successfully.");
+                    tmp = rec.getString("Input");
+                }
+            }
+            int start = tmp.indexOf('[');
+            int end = tmp.indexOf(']');
+            tmp = tmp.substring(start + 1, end);
+            String arrOfStr[] = tmp.split(", ");
+            input.addAll(Arrays.asList(arrOfStr));
+            return input;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public ArrayList getOutput(int questionID){
+        String sql = "SELECT * FROM Testcases";
+        String tmp = "";
+        ArrayList input = new ArrayList();
+        try (Connection connect = DriverManager.getConnection(
+                    "jdbc:mariadb://161.246.127.24:9004/clua7yac5000cbsmnfvj64hef", 
+                    "clua7yac3000absmnajq5d7jl", "kVGzhZ64mXAGplcwmtUL3Ub4");
+                Statement s = connect.createStatement();
+                ResultSet rec = s.executeQuery(sql);){
+            Class.forName("org.mariadb.jdbc.Driver");
+            System.out.println("Connected database successfully...");
+            while (rec.next()) {                
+                if (rec.getInt("ID") == questionID) {
+                    System.out.println("Get Output successfully.");
+                    tmp = rec.getString("Output");
+                }
+            }
+            int start = tmp.indexOf('[');
+            int end = tmp.indexOf(']');
+            tmp = tmp.substring(start + 1, end);
+            String arrOfStr[] = tmp.split(", ");
+            input.addAll(Arrays.asList(arrOfStr));
+            return input;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
