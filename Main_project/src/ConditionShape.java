4346e6080d93c59f30d8d066f7f446a63d3ae4ae
Pubeth <66070158@kmitl.ac.th>
2024-03-31 03:09:57 +0700

    Write converToCode for shape exclude input pls figure it out by yourself other parts is still in maintenance

--- Main_project/src/ConditionShape.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-import java.io.*;
-
-public abstract class ConditionShape extends ActionShape{
-    protected String condition;
-    public void convertSubCode(ArrayList<Shape> order, File f) {}
-}
-
