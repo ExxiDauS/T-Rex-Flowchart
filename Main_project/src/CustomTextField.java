90a0a1830decd7f2412510cc3d13af285ffc6797
Kuhn <88961962+KuhnphOn@users.noreply.github.com>
2024-04-01 02:18:14 +0700

    New! Add custom textfield and button

---
+++ Main_project/src/CustomTextField.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class CustomTextField extends JTextField {
+    public CustomTextField(){
+        setOpaque(false);
+        setColumns(22);
+        setFont(new Font("Montserrat", Font.PLAIN, 18));
+        setBorder(new EmptyBorder(10, 0, 10, 0));
+    }
+    public void setCol(int size){
+        setColumns(size);
+    }
+    public void setFontSize(int size){
+        setFont(new Font("Montserrat", Font.PLAIN, size));
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
+        int width = getWidth();
+        int height = getHeight();
+        g2.setColor(Color.BLACK);
+        g2.fillRect(2, height - 1, width - 4, 1);
+        g2.dispose();
+    }
+}
---
+++ Main_project/src/DoneCustomButton.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class DoneCustomButton extends JButton {
+    public DoneCustomButton(){
+        this("");
+    }
+    public DoneCustomButton(String text){
+        setOpaque(false);
+        setFocusPainted(false);
+        setContentAreaFilled(false);
+        setFont(new Font("Montserrat", Font.BOLD, 16));
+        setBorder(new EmptyBorder(10,0,10,0));
+        this.setText(text);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        Graphics2D g2 = (Graphics2D)g;
+        Graphics2D filling = (Graphics2D)g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
+        int width = getWidth();
+        int height = getHeight();
+        filling.setColor(new Color(76, 175, 80));
+        filling.fillRoundRect(3,3, width-5, height-5, 15, 15);
+//        g2.setStroke(new BasicStroke(2));
+//        g2.setColor(new Color(45,45,45));
+//        g2.drawRoundRect(2,2, width-5, height-5, 15, 15);
+        super.paint(g);
+        g2.dispose();
+        filling.dispose();
+    }
+}
